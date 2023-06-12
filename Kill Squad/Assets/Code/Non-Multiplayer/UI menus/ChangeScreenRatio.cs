e51d282ced396c5ae2c8084ef281aa65df761672
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2023-06-12 13:54:43 +0200

    Main menu improvements

---
+++ Kill Squad/Assets/Code/Non-Multiplayer/UI menus/ChangeScreenRatio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreenRatio : MonoBehaviour
+{
+    [SerializeField] Vector2[] resolutions;
+    public int resolutionIndex;
+    public bool isFullScreen;
+
+    public void ToggleFS(bool fs)
+    {
+        isFullScreen = fs;
+        ChangeResolution();
+    }
+    public void ChangeIndex(int index)
+    {
+        resolutionIndex = index;
+        ChangeResolution();
+    }
+    public void ChangeResolution()
+    {
+        Screen.SetResolution((int)resolutions[resolutionIndex].x, (int)resolutions[resolutionIndex].y, isFullScreen);
+    }
+}
