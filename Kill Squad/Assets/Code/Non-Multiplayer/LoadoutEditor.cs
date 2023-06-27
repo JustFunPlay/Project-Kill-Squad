5e85a6e958221898b551d59489399d1d05bb82a4
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2023-06-27 16:49:21 +0200

    Squad managaer wip

--- Kill Squad/Assets/Code/Non-Multiplayer/LoadoutEditor.cs
+++ Kill Squad/Assets/Code/Non-Multiplayer/LoadoutEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadoutEditor : MonoBehaviour
 {
@@ -8,62 +9,83 @@ public class LoadoutEditor : MonoBehaviour
     [SerializeField] private int currentSelectedUnitIndex;
 
     [SerializeField] private int currentPointTotal;
-    private PersistantInfo persistantInfo;
+    //private PersistantInfo persistantInfo;
     [SerializeField] private CharacterLoadout[] defaultCharacters;
+    [SerializeField] private List<CharacterLoadout> currentCharacters = new List<CharacterLoadout>();
+
+    [Header("UI")] 
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private TMPro.TextMeshProUGUI pointsCount;
 
     private void Start()
     {
-        persistantInfo = PersistantInfo.Instance;
+        currentCharacters = PersistantInfo.Instance.characters;
         UpdatePointsValue();
     }
     private void UpdatePointsValue()
     {
         currentPointTotal = 0;
-        for (int i = 0; i < persistantInfo.characters.Count; i++)
+        for (int i = 0; i < currentCharacters.Count; i++)
         {
-            CharacterInfoBase character = killSquad.squad[persistantInfo.characters[i].Character];
+            CharacterInfoBase character = killSquad.squad[currentCharacters[i].Character];
             currentPointTotal += character.pointsCost;
             for (int ii = 0; ii < (character.has3slots ? 3 : 2); ii++)
             {
-                currentPointTotal += character.weaponOptions[persistantInfo.characters[i].SelectedLoadoutOptions[ii]].pointsCost;
+                currentPointTotal += character.weaponOptions[currentCharacters[i].SelectedLoadoutOptions[ii]].pointsCost;
 
             }
-            switch (persistantInfo.characters[i].Character)
+            switch (currentCharacters[i].Character)
             {
                 case 2:
                     CommandoData comInfo = (CommandoData)character;
-                    if (persistantInfo.characters[i].SelectedLoadoutOptions[4] == 1)
-                        currentPointTotal += comInfo.grenades[persistantInfo.characters[i].SelectedLoadoutOptions[3]].extraCost;
+                    if (currentCharacters[i].SelectedLoadoutOptions[4] == 1)
+                        currentPointTotal += comInfo.grenades[currentCharacters[i].SelectedLoadoutOptions[3]].extraCost;
                     else
-                        currentPointTotal += comInfo.grenades[persistantInfo.characters[i].SelectedLoadoutOptions[3]].pointsCost;
+                        currentPointTotal += comInfo.grenades[currentCharacters[i].SelectedLoadoutOptions[3]].pointsCost;
                     break;
                 case 5:
-                    if (persistantInfo.characters[i].SelectedLoadoutOptions[2] == 1)
+                    if (currentCharacters[i].SelectedLoadoutOptions[2] == 1)
                         currentPointTotal += 20;
                         break;
             }
+        }
+        pointsCount.text = $"{currentPointTotal}/500";
+        if (currentPointTotal <= 500 && currentPointTotal >= 450)
+        {
+            pointsCount.color = Color.green;
+            confirmButton.interactable = true;
+        }
+        else
+        {
+            pointsCount.color = Color.red;
+            confirmButton.interactable = false;
         }
     }
 
     public void AddCharacter(int character = 0)
     {
-        persistantInfo.characters.Add(defaultCharacters[character]);
+        currentCharacters.Add(defaultCharacters[character]);
         UpdatePointsValue();
     }
     public void RemoveCharacter(int index)
     {
-        persistantInfo.characters.RemoveAt(index);
+        currentCharacters.RemoveAt(index);
         UpdatePointsValue();
     }
     public void ChangeCharacter(int index, int character)
     {
-        persistantInfo.characters[index] = defaultCharacters[character];
+        currentCharacters[index] = defaultCharacters[character];
         UpdatePointsValue();
     }
     public void ChangeEquipmentOption(int slot, int index)
     {
-        persistantInfo.characters[currentSelectedUnitIndex].SelectedLoadoutOptions[slot] = index;
+        currentCharacters[currentSelectedUnitIndex].SelectedLoadoutOptions[slot] = index;
         UpdatePointsValue();
+    }
+
+    public void ConfirmSquad()
+    {
+        PersistantInfo.Instance.characters = currentCharacters;
     }
 
 }
